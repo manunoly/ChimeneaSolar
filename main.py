a47acac217e9395dd9e22ef819a3af4d224b44d5
manunoly <manunoly@gmail.com>
2016-09-06 17:16:09 -0500

    rectificando bugs menores

--- main.py
+++ main.py
@@ -16,10 +16,13 @@ Tg = clima.Ta
 rangoTg = 2.0
 rangoSuperiorTg = Tg + rangoTg
 resultado = []
+
 To = Tg + rangoTg
-rangoTo = 15.0
-incremento = 0.02
+rangoTo = 20.0
+incremento = 0.05
 fijoTo = False
+cant = 0
+variacionProceso = 0.5
 
 def matar_procesos(pid):
     parent = psutil.Process(pid)
@@ -29,13 +32,13 @@ def matar_procesos(pid):
         print(child)
         child.kill()
     psutil.wait_procs(children, timeout=5)
-cant = 0
-i = To + 0.2
+
+i = To + variacionProceso
 limite = To + rangoTo
-while (i <= limite):
-    piscina.append(Process(target=TareaCalcular(i, cola, fijoTo, incremento, rangoSuperiorTg, clima, pared, vidrio).run))
-    # print("proceso" + str(i))
-    i = i + 0.2
+while (round(i ,3) <= limite):
+    piscina.append(Process(target=TareaCalcular(i, cola, fijoTo, incremento, rangoSuperiorTg, clima, pared, vidrio, variacionProceso).run))
+    # print("proceso " + str(i))
+    i = i + variacionProceso
     cant = cant + 1
 print("Creados " + str(cant) + " procesos")
 
@@ -54,30 +57,36 @@ while piscina:
     terminarProcesos()
     # Para no saturar, dormimos al padre durante 1 segundo
     #print("esperando a que los procesos hagan su trabajo")
-    time.sleep(1)
-    timeEnd = timeEnd + 1
-    if timeEnd > 150:
-        print("Tiempo límite exedido de 180 segundos, Fuerzo el cierre!!")
+    time.sleep(2)
+    timeEnd = timeEnd + 2
+    if timeEnd > 250:
+        print("Tiempo límite exedido de 150 segundos, Fuerzo el cierre!!")
         terminarProcesos()
         time.sleep(1)
         matar_procesos(os.getpid())
         break
 
-print("PADRE: todos los hijos han terminado, cierro")
+# print("PADRE: todos los hijos han terminado, cierro")
 valorFinal = 100.0
+menores = []
+cantV = 0
 while not cola.empty():
     valor = cola.get()
+    cantV = cantV + valor[0]
     if abs(valor[1]) < 1:
-        print(valor)
+        menores.append(valor)
     if abs(valor[1]) < abs(valorFinal):
         valorFinal = valor[1]
         datosF = valor
-
+print(cantV)
+exit()
 print("La mejor Temp To " + str(datosF[2]))
 print("La mejor Temp Tg " + str(datosF[3]))
 print("La mejor Temp Tf " + str(datosF[4]))
 # aproximado, hw, sw, hrwg
 print("Valor de aproximación " + str(datosF[1]))
+
+
 
 tiempoActual = 3600
 # To1 = (sw -(hw *(To -Tf) - (hrwg * (To - Tg)) - ((kp / x) * (To - T1))) * ((2 * 3600) / (cpp * densp * x))) + To
--- tareaCalcularProcesos.py
+++ tareaCalcularProcesos.py
@@ -3,7 +3,7 @@ from chimeneaSolar import ChimeneaSolar
 
 class TareaCalcular:
 
-    def __init__(self, cid, cola, fijoTo, incremento, rangoSuperiorTg, clima, pared, vidrio):
+    def __init__(self, cid, cola, fijoTo, incremento, rangoSuperiorTg, clima, pared, vidrio, variacionProceso):
         self.__cid=cid
         self.__cola = cola
         self.__fijoTo = fijoTo
@@ -12,6 +12,7 @@ class TareaCalcular:
         self.__clima = clima
         self.__pared = pared
         self.__vidrio = vidrio
+        self.variacionProceso = variacionProceso
         # print("HIJO {0} - Nace".format(self.__cid))
 
     # def __del__(self):
@@ -27,13 +28,22 @@ class TareaCalcular:
 
         if not self.__fijoTo:
             vueltas = 0
+            vueltasTo = 0
+            vueltasTg = 0
             To = self.__cid
-            rangoInferiorTo = To - 0.2
-            while To >= rangoInferiorTo:
+            rangoInferiorTo = round(To - self.variacionProceso, 3)
+            while round(To, 3) > rangoInferiorTo:
+                # print("To " + str(To))
+                vueltasTo = vueltasTo + 1
+
                 Tg = self.__clima.Ta
-                while Tg <= self.__rangoSuperiorTg:
+                while round(Tg, 3) <= self.__rangoSuperiorTg:
                     Tf = Tg
-                    while Tf <= To:
+                    vueltasTg = vueltasTg + 1
+                    # print("__tg " + str(Tg))
+                    while round(Tf,3) <= round(To, 3):
+                        # print("_____tf " + str(Tf))
+                        vueltas = vueltas + 1
                         a = chimenea.calcular(To, Tg, Tf)
                         if abs(a[0]) < menorValor:
                             menorValor = a[0]
@@ -41,8 +51,9 @@ class TareaCalcular:
                         Tf = Tf + self.__incremento
                     Tg = Tg + self.__incremento
                 To = To - self.__incremento
-            # print(str(self.__cid) + " Vueltas  " + str(vueltas))
+            # print(str(self.__cid) + " Rango Inferior " + str(rangoInferiorTo) +" Vueltas  " + str(vueltas) + " Valores Finale To " + str(vueltasTo) + " Tg " + str(vueltasTg) + " Tf " + str(vueltas))
+
             if menorValor != 100.0:
-                self.__cola.put([False, temperaturas[0], temperaturas[1], temperaturas[2], temperaturas[3], temperaturas[4]])
+                self.__cola.put([vueltas, temperaturas[0], temperaturas[1], temperaturas[2], temperaturas[3], temperaturas[4]])
                 # self.convergen(False, temperaturas)
 
